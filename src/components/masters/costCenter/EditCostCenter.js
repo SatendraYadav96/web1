84f15777ff6d36484240bf488b5c22640a227668
Aryaan Vedak <109820798+AryaanVedak@users.noreply.github.com>
2023-04-04 13:13:23 +0530

    create cost center and edit cost center

---
+++ src/components/masters/costCenter/EditCostCenter.js
@@ -0,0 +1,46 @@
+import React, {useState} from "react";
+import TitleWidget from "../../../widgets/TitleWidget";
+import PropTypes from "prop-types";
+import {selectAuthInfo} from "../../../redux/selectors/authSelectors";
+import {connect} from "react-redux";
+import {Button, Checkbox, Col, Input, Row} from "antd";
+
+const EditCostCenterComponent = ({authInfo}) => {
+
+    return(
+        <>
+            <TitleWidget title={"Edit Cost Center"}/>
+            <Row gutter={[8,8]}>
+                <Col span={8} offset={2}>
+                    Name: <Input placeholder={"Cost Center Name"}/>
+                </Col>
+                <Col span={8} offset={2}>
+                    Code: <Input placeholder={"Cost Center Code"}/>
+                </Col>
+                <Col span={4}></Col>
+                <Col span={8} offset={2}>
+                    IsActive: <Checkbox />
+                </Col>
+                <Col span={14}></Col>
+                <Col span={22}></Col>
+                <Col span={2}><Button type={"primary"}>Submit</Button></Col>
+            </Row>
+        </>
+    )
+
+}
+
+EditCostCenterComponent.propTypes = {
+    authInfo: PropTypes.any
+}
+
+const mapState = (state) => {
+    const authInfo = selectAuthInfo(state)
+    return {authInfo}
+}
+
+const actions = {
+
+}
+
+export default connect(mapState, actions) (EditCostCenterComponent)
