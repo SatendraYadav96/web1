fb6fc794598212d2db30897b836e1e5c1f2ccdfe
satendra yadav <89693080+SatendraYadav96@users.noreply.github.com>
2023-04-10 15:45:39 +0530

    u

--- src/components/reports/PurchaseReportComponent.js
+++ src/components/reports/PurchaseReportComponent.js
@@ -174,7 +174,7 @@ const PurchaseReportComponent = ({authInfo,profileInfo,purchaseList,purchaseRepo
                      From Date <br/><DatePicker value={startDate} onChange={(e) => setStartDate(e)} format={"DD/MM/YYYY"} defaultValue={moment().startOf('month')}/>
                  </Col>
                  <Col span={3}>
-                     To Date <br/><DatePicker value={endDate} onChange={(e) => setEndDate(e)} format={"DD/MM/YYYY"} defaultValue={now}/>
+                     To Date <br/><DatePicker value={endDate} onChange={(e) => setEndDate(e)} format={"DD/MM/YYYY"} defaultValue={moment().endOf('month')}/>
                  </Col>
                 <Col span={3}>
                     <br/>
