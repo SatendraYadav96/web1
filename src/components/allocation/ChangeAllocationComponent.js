92c69c4e7d3f698e2e3c319a47e1ef9e6ada402a
satendra yadav <89693080+SatendraYadav96@users.noreply.github.com>
2023-12-07 11:40:51 +0530

    updated

--- src/components/allocation/ChangeAllocationComponent.js
+++ src/components/allocation/ChangeAllocationComponent.js
@@ -13,6 +13,104 @@ const ChangeAllocationComponent = ({authInfo, profileInfo, item, planId, invento
     const [showErrorMessage, setShowErrorMessage] = useState(false)
     const [errorMessage, setErrorMessage] = useState('')
 
+
+    const [searchText, setSearchText] = useState('');
+    const [searchedColumn, setSearchedColumn] = useState('');
+    const searchInput = useRef(null);
+
+const handleSearch = (selectedKeys, confirm, dataIndex) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
+};
+const handleReset = (clearFilters) => {
+    clearFilters();
+    setSearchText('');
+};
+const getColumnSearchProps = (dataIndex) => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+        <div
+            style={{
+                padding: 8,
+            }}
+            onKeyDown={(e) => e.stopPropagation()}
+        >
+            <Input
+                ref={searchInput}
+                placeholder={`Search ${dataIndex}`}
+                value={selectedKeys[0]}
+                onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                style={{
+                    marginBottom: 8,
+                    display: 'block',
+                }}
+            />
+            <Space>
+                <Button
+                    type="primary"
+                    onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    icon={<SearchOutlined />}
+                    size="small"
+                    style={{
+                        width: 90,
+                    }}
+                >
+                    Search
+                </Button>
+                <Button
+                    onClick={() => clearFilters && handleReset(clearFilters)}
+                    size="small"
+                    style={{
+                        width: 90,
+                    }}
+                >
+                    Reset
+                </Button>
+                <Button
+                    type="link"
+                    size="small"
+                    onClick={() => {
+                        close();
+                    }}
+                >
+                    close
+                </Button>
+            </Space>
+        </div>
+    ),
+    filterIcon: (filtered) => (
+        <SearchOutlined
+            style={{
+                color: filtered ? '#1677ff' : '#1677ff',
+            }}
+        />
+    ),
+    onFilter: (value, record) =>
+        record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+        if (visible) {
+            setTimeout(() => searchInput.current?.select(), 100);
+        }
+    },
+    render: (text) =>
+        searchedColumn === dataIndex ? (
+            <Highlighter
+                highlightStyle={{
+                    backgroundColor: '#ffc069',
+                    padding: 0,
+                }}
+                searchWords={[searchText]}
+                autoEscape
+                textToHighlight={text ? text.toString() : ''}
+            />
+        ) : (
+            text
+        ),
+});
+
+
+
     const onChangeQuantity = (team, quantity) => {
         let qty = quantity.currentTarget.value
         if (qty % item.packSize !== 0) {
@@ -92,32 +190,38 @@ const ChangeAllocationComponent = ({authInfo, profileInfo, item, planId, invento
         {
             title: 'Zone',
             dataIndex: 'zone',
-            key: ''
+            key: '',
+            ...getColumnSearchProps('zone'),
         },
         {
             title: 'State',
             dataIndex: 'state',
-            key: ''
+            key: '',
+            ...getColumnSearchProps('state'),
         },
         {
             title: 'Designation',
             dataIndex: 'roleName',
-            key: ''
+            key: '',
+            ...getColumnSearchProps('roleName'),
         },
         {
             title: 'Person',
             dataIndex: 'recipientName',
-            key: ''
+            key: '',
+            ...getColumnSearchProps('recipientName'),
         },
         {
             title: 'Code',
             dataIndex: 'recipientCode',
-            key: ''
+            key: '',
+            ...getColumnSearchProps('recipientCode'),
         },
         {
             title: 'Headquarter',
             dataIndex: 'headQuarter',
-            key: ''
+            key: '',
+            ...getColumnSearchProps('headQuarter'),
         },
         {
             title: 'Qty',
@@ -137,6 +241,7 @@ const ChangeAllocationComponent = ({authInfo, profileInfo, item, planId, invento
             {showErrorMessage &&
                 <Alert message={errorMessage} type="error" />
             }
+            <span>Total Rows: <b>{teamForDifferentialAllocation?.length}</b></span>
                 <Table columns={columns} pagination={{pageSize: 6}} scroll={{y: 500}} dataSource={teamForDifferentialAllocation}></Table>
             {/*<Row gutter={[8,8]}>*/}
             {/*    <Col span={1} offset={23}><Button type={'primary'} onClick={() => SaveDifferentialAllocation()}>Save</Button></Col>*/}
